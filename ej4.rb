3003b5f6b7d5a4b35c50a91009dd94b233277887
tam <tamara.acevedoavila@gmail.com>
2018-05-11 00:37:53 -0300

    ej 4 todos

--- ej4.rb
+++ ej4.rb
@@ -4,59 +4,58 @@ file.close
 
 opcion = ''
 until opcion == 6 do
-  puts "Ingresa opcion: 1: cantidad de productos \n 2: stock de un producto \n 3: productos no bodega \n 4: productos con menos cant qe \n 5: registrar nuebo \n 6: salir \n "
+  puts "\nIngresa opcion:\n 1: cantidad de productos \n 2: stock de un producto \n 3: productos no bodega \n 4: productos con menos cant qe \n 5: registrar nuebo \n 6: salir \n "
   opcion = gets.chomp
   case opcion
   when "1"
     submenu = ''
     until submenu == 'd' do
-      puts "Ingresa opcion:\n a: Mostrar existencia por productos \n b: Mostrar la existencia total por tienda. \n c: Mostrar la existencia total en todas las tiendas. \n d: Volver a menu ppal \n "
-
+      puts "\nIngresa opcion:\n a: Mostrar existencia por productos \n b: Mostrar la existencia total por tienda. \n c: Mostrar la existencia total en todas las tiendas. \n d: Volver a menu ppal \n "
       submenu = gets.chomp
+
       case submenu
       when "a"
         # Mostrar la existencia por productos.
         pro = data.each do |linea|
-            valor = linea.split(' ')
-            suma = 0
-            valor.each_with_index do |dato, i|
-              if i != 0 && dato != 'NR'
-                dato = dato.to_i
-                suma += dato
-              end
+          valor = linea.gsub(',', '').split(' ')
+          suma = 0
+          valor.each_with_index do |dato, i|
+            if i != 0 && dato != 'NR'
+              dato = dato.to_i
+              suma += dato
             end
-            puts "#{valor[0]} = #{suma}\n"
+          end
+          puts "#{valor[0]} = #{suma}\n"
         end
 
       when "b"
         # Mostrar la existencia total por tienda.
         existencias = {"bodega1" => 0,"bodega2" => 0,"bodega3" => 0}
         pro = data.each do |linea|
-            valor = linea.split(' ')
+            valor = linea.gsub(',', '').split(' ')
             suma = 0
             valor.each_with_index do |dato, i|
-
                if i != 0 && dato != 'NR'
                  dato = dato.to_i
                  existencias["bodega#{i}"] +=  dato
                end
             end
         end
-        puts existencias
+        puts "Bodega 1 tiene: #{existencias['bodega1']}"
+        puts "Bodega 2 tiene: #{existencias['bodega2']}"
+        puts "Bodega 3 tiene: #{existencias['bodega3']}"
 
       when "c"
         #) Mostrar la existencia total en todas las tiendas.
         suma = 0
-         pro = data.each do |linea|
+        pro = data.each do |linea|
             valor = linea.split(' ')
-
             valor.each_with_index do |dato, i|
               if i != 0 && dato != 'NR'
                 dato = dato.to_i
                 suma += dato
               end
             end
-
         end
         puts "Total productos en todas las tiendas = #{suma}\n"
 
@@ -68,47 +67,44 @@ until opcion == 6 do
     end
 
   when "2"
-  # permite que el usuario ingrese el nombre de un producto y el programa responderá con la cantidad de stock total (suma en las bodegas) de ese producto.
-  puts "ingrese el nombre de un producto para contar su stock"
-  productostock = gets.chomp
+    # permite que el usuario ingrese el nombre de un producto y el programa responderá con la cantidad de stock total (suma en las bodegas) de ese producto.
+    puts "ingrese el nombre de un producto para contar su stock"
+    productostock = gets.chomp
 
-  pro = data.each do |linea|
+    pro = data.each do |linea|
       valor = linea.gsub(',', '').split(' ')
-      suma = 0
-
+        suma = 0
         if valor[0] == productostock
-
-        valor.each_with_index do |dato, i|
+          valor.each_with_index do |dato, i|
           if i != 0 && dato != 'NR'
             dato = dato.to_i
             suma += dato
           end
         end
-          puts "#{valor[0]} = #{suma}\n" if valor[0] == productostock
+        puts "#{valor[0]} = #{suma}\n" if valor[0] == productostock
       end
-  end
+    end
+
   when "3"
     # muestra los productos no registrados en cada bodega.
-    nr = {"bodega1" => 0,"bodega2" => 0,"bodega3" => 0}
+    nr = {"bodega1" => '',"bodega2" => '',"bodega3" => ''}
     pro = data.each do |linea|
-        valor = linea.split(' ')
-        suma = 0
+        valor = linea.gsub(',', '').split(' ')
         valor.each_with_index do |dato, i|
-
            if  dato == 'NR'
-
-             existencias["bodega#{i}"] = + "#{dato[0]} "
+             nr["bodega#{i}"] += "#{valor[0]} "
            end
         end
     end
-    puts nr
+    puts "Bodega 1 no registra: #{nr['bodega1']}"
+    puts "Bodega 2 no registra: #{nr['bodega2']}"
+    puts "Bodega 3 no registra: #{nr['bodega3']}"
   when "4"
     # permite conocer los productos con una existencia total menor a un valor ingresado por el usuario.
-
     puts "ingrese un valor y mostraremos los productos con menor existencia"
     menora = gets.chomp.to_i
     pro = data.each do |linea|
-    valor = linea.split(' ')
+      valor = linea.split(' ')
       suma = 0
       valor.each_with_index do |dato, i|
         if i != 0 && dato != 'NR'
@@ -118,7 +114,7 @@ until opcion == 6 do
       end
 
       puts "#{valor[0]} = #{suma}\n" if suma < menora
-  end
+    end
 
   when "5"
     # permite registrar un nuevo producto con su respectivo stock en cada bodega. (Hint: abrir el archivo como append).
@@ -136,7 +132,6 @@ until opcion == 6 do
       puts "producto ingresado:  #{name}, #{bodega1}, #{bodega2}, #{bodega3}\n"
       f.close
     }
-
 
   when "6"
     puts 'Adios'
